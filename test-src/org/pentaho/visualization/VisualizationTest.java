caa042540bbb8a0a6c0110660da6b6db7206d48c
pminutillo <pminutillo@pentaho.com>
2014-11-25 10:57:36 -0500

    Added runnable method to test for ci

--- test-src/org/pentaho/visualization/VisualizationTest.java
+++ test-src/org/pentaho/visualization/VisualizationTest.java
@@ -1,7 +1,12 @@
 package org.pentaho.visualization;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class VisualizationTest {
+
+	@Test
+	public void testPlaceholder(){
+	}
 
 }
